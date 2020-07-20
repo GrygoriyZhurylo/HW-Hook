23b622aba76c002136dfec3323d55e9f4ebaafb3
Greg <grygoriy23@gmail.com>
2020-07-19 22:24:22 -0500

    HW

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [count10, setCount10] = useState(0);
+    const [rand, setRand] = useState(0);
+
+    const add = () => {
+        setCount(count + 1);
+    }
+    const minus = () => {
+        setCount(count - 1);
+    }
+    const add10 = () => {
+        setCount10(count10 + 10);
+    }
+    const minus10 = () => {
+        setCount10(count10 - 10);
+    }
+    const ran = () => {
+        setRand(Math.floor(Math.random() * 11));
+    }
+    const minusrand = () => {
+        setRand(Math.floor(Math.random() * 11));
+    }
+    return (
+        <div className="App">
+            <h3>
+                <button onClick={add} className="btn btn-outline-primary">Add +1</button>
+                {count}
+                <button onClick={minus} className="btn btn-outline-primary">Delete -1</button>
+                <br/>
+                <button onClick={add10} className="btn btn-outline-primary">Add +10</button>
+                {count10}
+                <button onClick={minus10} className="btn btn-outline-primary">Delete -10</button>
+                <br/>
+                <button onClick={ran} className="btn btn-outline-primary">Random from 1 to 10</button>
+                {rand}
+                <button onClick={minusrand} className="btn btn-outline-primary">Random from 1 to 10</button>
+            </h3>
+        </div>
+    );
 }
 
 export default App;
