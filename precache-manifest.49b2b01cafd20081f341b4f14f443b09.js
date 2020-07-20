d31ea6323ea12b8dc0152841e10c191056917545
Greg <grygoriy23@gmail.com>
2020-07-20 12:28:09 -0500

    Updates

---
+++ precache-manifest.49b2b01cafd20081f341b4f14f443b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c430e04a2fea9dd8f8a7b1c7e3add454",
+    "url": "/HW-Hook/index.html"
+  },
+  {
+    "revision": "276f0f6241295c525a36",
+    "url": "/HW-Hook/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1be6af083603302cb41c",
+    "url": "/HW-Hook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "276f0f6241295c525a36",
+    "url": "/HW-Hook/static/js/2.5a307afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW-Hook/static/js/2.5a307afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be6af083603302cb41c",
+    "url": "/HW-Hook/static/js/main.7a74e487.chunk.js"
+  },
+  {
+    "revision": "2ecb6ebfb912bb203070",
+    "url": "/HW-Hook/static/js/runtime-main.986529de.js"
+  }
+]);
